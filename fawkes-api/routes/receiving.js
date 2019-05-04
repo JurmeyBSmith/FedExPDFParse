0151a61467d325b42a456ce2bff59524130b7745
Jeremy Smith <jeremy.smith@fawkesengineering.com>
2019-05-03 19:34:19 -0700

    cleanup

--- fawkes-api/routes/receiving.js
+++ fawkes-api/routes/receiving.js
@@ -6,50 +6,35 @@ module.exports = (app, connection) => {
 
     app.post('/api/receiving', function (req, res) {
 
-        //================Mitchell test code ===================
-        // var trackNumber = req.body.trackingNumber;
-        // this.getThePDF(trackNumber, (a_filePath) => {
-        //     this.parsePDF(a_filePath, (a_jsonObjectResults) => {
-        //         this.saveDataBase(a_jsonObjectResults, (results) => {
-        //             res.send();
-        //         })
-        //     })
-        // })
-
-
-        // function getThePDF(trackingNumber, a_callback) {
-        //     // do something
-
-        //     var filePath = 'myFile';
-        //     a_callback(filePath);
-        // }
-
-        //======================================================
-        var year = 2019;
+        var date = new Date();
+        var year = 2019;//date.getYear()+1900;
         const trackingNumber = req.body.trackingNumber;
         const pdfPath = "routes/pdfFiles/" + trackingNumber + ".pdf";
         const strBaseFedEx = "https://www.fedex.com/trackingCal/retrievePDF.jsp?trackingNumber=" + trackingNumber + "&trackingQualifier=1" + year + "~" + trackingNumber + "~FDEG&trackingCarrier=FDXG&shipDate=&destCountry=&locale=en_US&accountNbr=148750947&type=SPOD&appType=&anon=";
         var text;
         var textNum;
+        
         // make fedex request
-
-        get_POD(year, strBaseFedEx, pdfPath, (a_pathToPOD) => {
+        get_POD(year, strBaseFedEx, pdfPath, (a_pathToPOD, a_bRetry) => {
+            if (a_bRetry) {
+                console.log('We are retrying cause we failed.')
+            }
             parse_POD(a_pathToPOD, (a_dataText) => {
                 //console.log("After get data: ", a_dataText);
                 text = a_dataText.toLowerCase().match(/(:+|[a-z]+|\S+)/gi);
                 var textSemicolon = a_dataText.split(/\s*(?::|$)\s*/);
                 textNum = a_dataText.match(/\d+/g);
-                var trackNumText = textNum[2];
-                var deliverMonth = text[33];
-                var deliverDay = textNum[3];
-                var deliverYear = textNum[4];
-                var reference = textNum[14];
-                var shipLb = textNum[10];
-                var shipOz = textNum[11];
-                var delCity = text[23];
-                var delState = text[25];
+                // var trackNumText = textNum[2];
+                // var deliverMonth = text[33];
+                // var deliverDay = textNum[3];
+                // var deliverYear = textNum[4];
+                // var reference = textNum[14];
+                // var shipLb = textNum[10];
+                // var shipOz = textNum[11];
+                // var delCity = text[23];
+                // var delState = text[25];
                 //console.log("\n\nText: \n\n", text, "\n\n Text Nmbers: \n\n", textNum, "\n\n Semi colons: \n\n", textSemicolon);
-                // console.log("\n\nGetting data out of PDF...\n\n\nTracking Number:", trackNumText, 
+                //console.log("\n\nGetting data out of PDF...\n\n\nTracking Number:", trackNumText, 
                 //     "\nDelivery Date:", deliverMonth, deliverDay, deliverYear, 
                 //     "\nReference: #", reference, 
                 //     "\nShipping Weight:", shipLb,'lb.',shipOz, 'oz.',
@@ -58,39 +43,15 @@ module.exports = (app, connection) => {
                 connection.query(pull_Data(a_dataText), function (err, result) {
                     if (err) throw err;
                     console.log("Post Successful");
+                    console.log("Year: ", date.getYear()+1900);
                     res.send(result)
-                  });
+                });
             })
-        })
 
+           
+        })
     });
 
-
-    //     const file = fs.createWriteStream(pdfPath);
-    //     axios({ // async
-    //         method: 'get',
-    //         url: strBaseFedEx,
-    //         responseType: 'stream'
-    //     })
-    //         .then(response => {
-    //             response.data.pipe(file);
-    //             // parse the pod
-    //             var podData = get_data(pdfPath)
-    //             // insert into the database
-    //             var sql = `INSERT INTO \`shipment\` (\`tracking_number\`, \`reference_number\`, \`store\`, \`street\`, \`state\`, \`zip\`, \`city\`, \`country\`, \`status\`, \`delivered_date\`, \`received_date\`) VALUES (\'${trkNm}\', \'r3f3r3nc3numb3r\', \'store\', \'street\', \'${podData.state}\', \'92592\', \'Escondido\', \'US\', \'Passed\', \'1996-04-01\', \'1996-04-01\')`;
-    //             connection.query(sql, function (err, result) {
-    //                 if (err) {
-    //                     res.status(400).send({ message: 'Something went wrong, when entering into the database. Please contact Mr. Smith. ', success: false, data: err });
-    //                     throw err;
-    //                 }
-    //                 console.log("Post request successful!");
-    //                 res.status(200).send({ message: 'POD recieved and prased. ', success: true, data: podData });
-    //             });
-    //         }).catch(err => {
-    //             console.log(err)
-    //             res.status(400).send({ message: 'Something went wrong. Please contact Mr. Smith. ', success: false, data: err });
-    //         })
-    // })
 
     app.get('/api/receiving', (req, res) => {
         console.log('This is the get request from receiving');
@@ -99,6 +60,7 @@ module.exports = (app, connection) => {
 
 
     function get_POD(year, strBaseFedEx, pdfPath, a_callback) {
+        console.log("CALLED GET_POD");
         //request to fedex for pdf
         const file = fs.createWriteStream(pdfPath);
         axios({ // async
@@ -114,45 +76,34 @@ module.exports = (app, connection) => {
                 });
             }).catch(err => {
                 console.log(err)
-                // for (year; year > 2015; year--) {
-
-                // }
+                get_POD(year-1, strBaseFedEx, pdfPath, a_callback);
             })
-        // pdfTOTEXT and then walk that data and get what we need out. 
-
-        //var podData = { address: '126 foo road', state: 'CA' };
-
-        //return podData;
+    
     }
 
     //function parse_pdf()
     function parse_POD(pdfPath, a_callback) {
+        console.log("CALLED PARSE_POD");
         var dataBuffer = fs.readFileSync(pdfPath);
         pdf(dataBuffer) // async
             .then(data => {
-                console.log('PDF->Text');
-                // number of pages
-                // console.log(data.numpages);
-                // number of rendered pages
-                // console.log(data.numrender);
-                // PDF info
-                // console.log(data.info);
-                // PDF metadata
-                // console.log(data.metadata);
-                // PDF.js version
-                // check https://mozilla.github.io/pdf.js/getting_started/
-                // console.log(data.version);
-                // PDF text
-                // console.log(data.text);
-                //get_data(strBaseFedEx, trackingNumber, pdfPath);
-                a_callback(data.text);
-                text = data.text;
+                console.log("THis is the data", data)
+                if (data) {
+                    a_callback(data.text);
+                    text = data.text;
+                } else {
+                    var err = {message: "We dont have a good pdf"};
+                    throw err;
+                }
             }).catch(err => {
-                console.log(err)
+                // We got a bad pdf                
+                console.log("This is the err:", err)
+                a_callback(null, true);
             });
     }
 
     function pull_Data(a_dataText) {
+        //console.log("CALLED PULL_DATA");
         text = a_dataText.toLowerCase().match(/(:+|[a-z]+|\S+)/gi);
         //variables
         var TrackingNumber;
@@ -165,18 +116,7 @@ module.exports = (app, connection) => {
         var ShipCity;
         var ShipState;
         var ShipCountry;
-        // text.forEach(element => {
-
-        //     switch(element) {
-        //         case "tracking":
-        //             //get_TrackingNumber(element);   // do something with the element
-        //             console.log(element);
-        //             break;
-        //         default:
-        //             console.log("default");
-        //             break;
-        //     }
-        // });
+        
 
         for (var i = 0; i < text.length; i++) {
             switch (text[i]) {
@@ -260,7 +200,7 @@ module.exports = (app, connection) => {
     };
     function get_Reference(text, i) {
         return text[i + 1].replace( 'number' , '').toUpperCase() + text[i + 2].replace( '#' , '').toUpperCase();
-        return 
+        
     };
     function convert_Date(text1) {
         switch(text1) {
@@ -306,24 +246,4 @@ module.exports = (app, connection) => {
         }
         return text1;
     }
-    
-    // function get_Shipper(text, i){
-    //     console.log("Shipper =", )
-    // };
-    // function get_ReferenceNumber(text, i){
-
-    // };
-    // function get_ShipDate(text, i){
-
-    // };
-    // function get_Shipper(text, i){
-
-    // };
-
-    // function get_(text, i);
-
-
-
-
-
 }
