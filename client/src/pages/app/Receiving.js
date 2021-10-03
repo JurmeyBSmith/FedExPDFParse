2c770dd30861e6fd2f0cda9d5f2de877490d695d
JurmeyBSmith <42648629+JurmeyBSmith@users.noreply.github.com>
2021-10-03 10:03:32 -0700

    Update Receiving.js

--- client/src/pages/app/Receiving.js
+++ client/src/pages/app/Receiving.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './css/Receiving.css';
-//import { Header, Segment } from 'semantic-ui-react'
 import axios from 'axios';
-//import { Menu } from 'semantic-ui-react';
 import { Redirect } from 'react-router-dom';
-//import NavBar from './navBar.js';
-//import ReactTooltip from 'react-tooltip'
 import Edit from './Edit';
 import './css/Receiving.css';
 
@@ -31,15 +26,6 @@ class Receiving extends Component {
 
   handleChange(event) {
     this.setState({ value: event.target.value,  });
-    //console.log("lenght: ", this.state.value.length)
-    //Error UI
-    // if(this.state.value.length !== 12){
-    //   this.setState({type: "ui error icon input"})
-    // }else if(isNaN(this.state.value)){
-    //   this.setState({type: "ui error icon input"})
-    // }else {
-    //   this.setState({type: "ui icon input"})
-    // }
   }
   handleSubmit(event) {
     //  do async request
@@ -66,7 +52,6 @@ class Receiving extends Component {
       trackingNumber: trackingNumber
     })
       .then((response) => {
-        //console.log("Response: ",response.data.message);
         if (response.data.message === 'Invalid PDF structure') {
           console.log("ERROR in client");
           //axios post again
@@ -86,7 +71,7 @@ class Receiving extends Component {
             console.log('The state is: ', this.state);
           })
             .catch(err => {
-              console.log("Yeaaah.. Post never went through")
+              console.log("Post never went through")
             })
         } else {
           this.setState({ jsonData: response })
@@ -98,13 +83,10 @@ class Receiving extends Component {
       .catch(function (error) {
         console.log("This is a 400 Err: ", error);
       });
-
-    //alert('Barcode "' + this.state.value + '", was submitted');
     event.preventDefault();
   }
   }
   render() {
-    //const { activeItem } = this.state
    
     if (this.state.step === 1) {
       return (
@@ -126,24 +108,11 @@ class Receiving extends Component {
 
       );
     }
-    // if (this.state.step === 2) {
-    //   return (
-    //     <Step2 {this.state.returnedData} />
-    //   )
-    // }
-    // if(this.state.toEdit === true) {
-    //   console.log(this.state)
-    //   return <Redirect to='/edit' />
-    // }
+
     if (this.state.step === 2) {
       console.log(this.state)
       return <Edit jsonData={this.state.jsonData} />
     }
-
-    // if ( this.state.step === "Step1Err") {
-    //   <Step1 {this.trac}/>
-    //   <Step1Err />
-    // }
 
   }
 }
