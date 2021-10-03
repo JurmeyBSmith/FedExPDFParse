f3bf419c61a7a635ba55a9eaef22d9a582e2a863
JurmeyBSmith <42648629+JurmeyBSmith@users.noreply.github.com>
2021-10-03 09:50:19 -0700

    Update server.js

--- fawkes-api/server.js
+++ fawkes-api/server.js
@@ -24,10 +24,8 @@ var connection = mysql.createConnection({
 })
 
 app.use(express.static('routes/pdfFiles'));
-//require("./routes/receiving")(app, connection);
 require("./routes/receivingv2")(app, connection);
 require("./routes/databaseInit")(app, connection);
-//require("./routes/trackingNumber")(app, connection);
 
 connection.connect((err) => {
   console.log('Are we even running this?')
@@ -44,10 +42,6 @@ connection.connect((err) => {
     });
   }
 });
-
-app.get('/', (req, res) => {
-  res.send('AT /');
-})
 
 app.get('/id', function(req, res) {
   var sql = 'SELECT \`test\`.\`id\` FROM \`test\`';
@@ -76,55 +70,6 @@ app.delete('/id', function(req, res) {
     res.send('DELETED');
   });
 })
-
-
-//get.axios test
-// app.get('/axios', (req, res) => {
-//   var URL = 'https://www.fedex.com/trackingCal/retrievePDF.jsp?trackingNumber=786292048841&trackingQualifier=12019~786292048841~FDEG&trackingCarrier=FDXG&shipDate=&destCountry=&locale=en_US&accountNbr=148750947&type=SPOD&appType=&anon=';
-// axios.get(URL)
-// .then(data=>console.log(data))
-// .catch(err=>console.log(err))
-
-// })
-// //PDFTOTEXTEST
-// app.get('/pdf', (req, res) => {
- 
-//   pdf(dataBuffer).then(function(data) {
- 
-//     // number of pages
-//     console.log(data.numpages);
-//     // number of rendered pages
-//     console.log(data.numrender);
-//     // PDF info
-//     console.log(data.info);
-//     // PDF metadata
-//     console.log(data.metadata); 
-//     // PDF.js version
-//     // check https://mozilla.github.io/pdf.js/getting_started/
-//     console.log(data.version);
-//     // PDF text
-//     console.log(data.text); 
-      
-//     console.log("This is the link");
-//     console.log(strBaseFedEx);
-// });
-
-  // var pdfUtil = require('pdf-to-text');
-  // var pdf_path = "routes/retrievePDF.pdf";
-  
-
-  // pdfUtil.info(pdf_path, function(err, data) {
-  //   if (err) {
-  //     console.log("Ther error was:", err);
-  //     throw(err)
-  //   } else {
-  //     console.log(data); //print all text 
-  //     res.send("completed the call;")
-  //   }    
-  // }).catch(function(err) {
-
-  // });
-//});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
